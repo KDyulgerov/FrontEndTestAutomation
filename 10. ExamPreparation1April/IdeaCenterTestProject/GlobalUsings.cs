d1f4b56736f85c6e09a5f12aef379fa0dfc0ea1c
KDyulgerov <kdyulgerov.sport@gmail.com>
2025-04-03 15:23:48 +0300

    Uploading Exam Prepration for April

---
+++ 10. ExamPreparation1April/IdeaCenterTestProject/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using OpenQA.Selenium;
+global using OpenQA.Selenium.Support.UI;
+global using SeleniumExtras.WaitHelpers;
+global using IdeaCenterTestProject.Pages;
+global using OpenQA.Selenium.Chrome;
+
