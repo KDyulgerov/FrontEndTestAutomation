d39d6c02add9b2eaf25d674d481000efebe5660e
KDyulgerov <kdyulgerov.sport@gmail.com>
2025-04-03 15:21:44 +0300

    Uploading Exam Preparation for April

---
+++ 10. ExamPreparationApril/01. ExamPreparation1/IdeaCenterTestProject/Pages/LoginPage.cs
@@ -0,0 +1,33 @@
+﻿namespace IdeaCenterTestProject.Pages
+{
+    public class LoginPage : BasePage
+    {
+        public LoginPage(IWebDriver driver) :base(driver)
+        {
+        }
+
+        public static readonly string PageUrl = BaseUrl + "/Users/Login";
+
+        // Locating login page elements
+
+        protected readonly By emailInput = By.XPath("//input[@type='email']");
+
+        protected readonly By passwordInput = By.XPath("//input[@type='password']");
+
+        protected readonly By signInButton = By.XPath("//button[@class='btn btn-primary btn-lg btn-block']");
+
+        // Methods
+
+        public void OpenPage()
+        {
+            driver.Navigate().GoToUrl(PageUrl);
+        }
+
+        public void Login(string email, string password)
+        {
+            Type(emailInput, email);
+            Type(passwordInput, password);
+            Click(signInButton);
+        }
+    }
+}
---
+++ 10. ExamPreparationApril/01. ExamPreparation1/IdeaCenterTestProject/Tests/BaseTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace IdeaCenterTestProject.Tests
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+
+        protected BasePage basePage;
+
+        protected LoginPage loginPage;
+
+        protected CreateIdeaPage createIdeaPage;
+
+        protected MyIdeasPage myIdeasPage;
+
+        protected ReadIdeaPage readIdeaPage;
+
+        protected EditIdeaPage editIdeaPage;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddUserProfilePreference("profile.password_manager_enabled", false);
+            options.AddArgument("--disable-search-engine-choice-screen");
+            options.AddArgument("--disable-features=PasswordLeakDetection");
+            options.AddArgument("guest");
+
+            driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            basePage = new BasePage(driver);
+            loginPage = new LoginPage(driver);
+            createIdeaPage = new CreateIdeaPage(driver);
+            myIdeasPage = new MyIdeasPage(driver);
+            readIdeaPage = new ReadIdeaPage(driver);
+            editIdeaPage = new EditIdeaPage(driver);
+
+            loginPage.OpenPage();
+            loginPage.Login("alabala@example.com", "123456");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+
+        public string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
